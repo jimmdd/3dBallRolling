452fa0c3199036e2eb4e1e356cacbd87728c1d22
jimmdd <jding@Jingluns-MacBook-Pro.local>
2016-10-22 00:19:18 +0200

    added collectible and script
    now coin is collectible and emerald is for ball size change. Put ball
    size change script into collectible

--- Assets/Script/Collectable.cs
+++ Assets/Script/Collectable.cs
@@ -1,18 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Coin : MonoBehaviour {
+public class Collectable : MonoBehaviour {
 	HUD hud;
 	void start(){
 		transform.RotateAround (transform.position, transform.up, Time.deltaTime * 90f);
 	}
 
 	void OnTriggerEnter(Collider other){
-		if (other.tag == "Player") {
+		if(other.tag =="Player"){
+		if (this.tag == "Coin" ) {
 			//for display increased score
 			hud = GameObject.Find ("Camera").GetComponent<HUD> ();
+			//TO-DO decrease from time
 			hud.IncreaseScore (20);
 			Destroy (this.gameObject);
+		}
+
+			if(this.tag =="Emerald"){
+				// do ball size change
+				Destroy(this.gameObject);
+			}
 		}
 	}
 }
