a34ce15c1d15e36b6895c02bcb6e0033e55a80ad
ryankenney42 <ryankenney42@hotmail.com>
2016-10-23 18:40:43 +0200

    Main Menu

--- Assets/Script/Collectable.cs
+++ Assets/Script/Collectable.cs
@@ -1,17 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
-
 public class Collectable : MonoBehaviour {
 	HUD hud;
-	//	void start(){
-	//		transform.RotateAround (transform.position, transform.up, Time.deltaTime * 90f);
-	//	}
-
+	//  void start(){
+	//      transform.RotateAround (transform.position, transform.up, Time.deltaTime * 90f);
+	//  }
 	void update(){
 		//make the gameobject rotate itself
 		transform.Rotate(new Vector3(0,Time.deltaTime*50,0));
 	}
-
 	void OnTriggerEnter(Collider other){
 		if(other.tag =="Player"){
 			if (this.tag == "Coin" ) {
@@ -21,19 +18,19 @@ public class Collectable : MonoBehaviour {
 				hud.IncreaseScore (20);
 				Destroy (this.gameObject);
 			}
-
 			if(this.tag =="Emerald"){
 				//ball size change smaller
-				if (other.transform.localScale.x > 1.0f) {
+				if (other.transform.localScale.x > .5f) {
+					other.GetComponent<PlayerController> ().jumpForce = 1000f;
 					other.transform.localScale += new Vector3 (-0.5f, -0.5f, -0.5f);
 					norotate.translate = -.51f;
 					Destroy (this.gameObject);
 				}
 			}
-
 			if (this.tag == "Ruby") {
 				//ball size change bigger
-				if (other.transform.localScale.x < 1.5f) {
+				if (other.transform.localScale.x < 1f) {
+					other.GetComponent<PlayerController> ().jumpForce = 1600f;
 					other.transform.localScale += new Vector3 (0.5f, 0.5f, 0.5f);
 					norotate.translate = -1f;
 					Destroy (this.gameObject);
